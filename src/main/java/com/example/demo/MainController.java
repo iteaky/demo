40f92ae99eb7be9c9ea2edc4d75c137ef4f6f0a9
iteaky <iteaky@gmail.com>
2020-05-05 21:54:15 +0300

    ping controller

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -3,25 +3,25 @@ package com.example.demo;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletRequest;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.io.Writer;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @RestController
 public class MainController {
 
-    @PostMapping(path = "/parse", headers ={"content-type=multipart/form-data"}, consumes = "multipart/form-data")
+    @GetMapping("/ping")
+    public String ping() {
+        return "OK";
+    }
+
+    @PostMapping(path = "/parse", headers = {"content-type=multipart/form-data"}, consumes = "multipart/form-data")
     public String parseImage(@RequestParam("file") MultipartFile file) throws IOException {
         System.out.println(file);
         BufferedImage bImageFromConvert = ImageIO.read(file.getInputStream());
@@ -30,17 +30,14 @@ public class MainController {
 
     public String getImgText(BufferedImage file) {
         ITesseract instance = new Tesseract();
-        try
-        {
+        try {
             instance.setDatapath("src/main/resources");
             instance.setLanguage("rus");
             instance.setPageSegMode(1);
             instance.setOcrEngineMode(1);
             String imgText = instance.doOCR(file);
             return imgText;
-        }
-        catch (TesseractException e)
-        {
+        } catch (TesseractException e) {
             e.getMessage();
             return "Error while reading image";
         }
